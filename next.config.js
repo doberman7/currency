e6fad9074f8c6eae6171204e193a08120ea220d2
doberman7 <ivanrubioangeles@gmail.com>
2022-03-21 19:52:06 -0600

    deletes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,2 @@
 const dotenv = require('dotenv');
-// TO DO move to env
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
-}
-module.exports = {  
-  // reactStrictMode: true,
-  
-  env: {
-    //  ACCESS_KEY:process.env.ACCESS_KEY//necesario para que el env file sea leido
-  },
-}
+
